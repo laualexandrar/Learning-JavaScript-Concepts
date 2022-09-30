be66297883f8b0abbfd3b50ea506474c1bac2b81
Laura R <laualexandrar@gmail.com>
2022-09-29 17:37:48 -0700

    course vite

---
+++ vite-course/vite-demo/main.js
@@ -0,0 +1,25 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
+import buttonStyle from './button.module.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+    <button id="btn">Click!</button>
+  </div>
+`
+document.getElementById('btn').className = buttonStyle.btn
+setupCounter(document.querySelector('#counter'))
