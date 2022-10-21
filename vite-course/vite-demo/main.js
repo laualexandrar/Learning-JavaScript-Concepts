d3337837412b31294648c866b342e1c5ad7fff33
Laura R <laualexandrar@gmail.com>
2022-10-20 22:47:09 -0700

    vite-demo

--- vite-course/vite-demo/main.js
+++ vite-course/vite-demo/main.js
@@ -2,6 +2,20 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 import buttonStyle from './button.module.css'
+import imageStyles from './image.module.css'
+import img from './vite-logo.png'
+import data from './data.json'
+const modules = import.meta.glob('./modules/*.js')
+import suma from './suma.ts'
+console.log(`suma 2 + 3 = ${suma(2,3)}`);
+
+console.log(modules)
+
+for(const path in modules) {
+  modules[path]().then((module) => {
+    module.load()
+  })
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -19,7 +33,13 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
     <button id="btn">Click!</button>
+    <img id="img" />
+    <pre>${JSON.stringify(data)}</pre>
   </div>
 `
 document.getElementById('btn').className = buttonStyle.btn
 setupCounter(document.querySelector('#counter'))
+
+const imagen = document.getElementById('img')
+imagen.src = img
+imagen.className = imageStyles.img
