e6e2c4f9882d92dead0721fbf3daaf1b188359d1
Laura R <laualexandrar@gmail.com>
2022-10-26 16:37:07 -0700

    Vite course 1.1

---
+++ vite-course/vite-demo/vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig, loadEnv } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig( ({command, mode}) => {
+    const port = 3000
+
+    console.log(command, mode)
+    const env = loadEnv(mode, process.cwd())
+    console.log(env.VITE_NAME)
+    if(mode === "development") {
+        console.log("Modo desarrollo")
+        return {
+        server: { 
+        port:8080
+        }
+    }
+    } else {
+        console.log("modo produccion")
+        return {
+            build: {
+                rollupOptions: {
+                    input: {
+                        main: resolve(__dirname, 'index.html'),
+                        help: resolve(__dirname, 'help', 'help.html')
+                    }
+                }
+            }
+        }
+    }
+})
